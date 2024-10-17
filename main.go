061cdc97baa47b51224058e22d3532cbdb951018
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-10-18 01:10:29 +0600

    added all crud operations

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-crud/controllers"
 	"go-crud/initializers"
 )
 
@@ -13,11 +14,11 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/posts", controllers.PostsCreate)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 
 	r.Run()
 }
