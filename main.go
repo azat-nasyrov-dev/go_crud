44a1971c15147c37b761bf8f8f437f649a4a47af
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-10-17 22:47:46 +0600

    added env variables and config for app

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"github.com/gin-gonic/gin"
+	"go-crud/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
